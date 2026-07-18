f0070a2266441e1c7fdcc35f5664e74c0b69cf0d
apptade <apptade64@gmail.com>
2026-07-18 19:14:33 +0300

    Initial commit

---
+++ CR.Exceptions/CrException.cs
@@ -0,0 +1,11 @@
+﻿namespace CR.Exceptions;
+
+public abstract class CrException : Exception
+{
+    public string Code { get; }
+
+    protected CrException(string code, string message, Exception? innerException = null) : base(message, innerException)
+    {
+        Code = code;
+    }
+}
