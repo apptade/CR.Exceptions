e168bf249f26a40fc7d7b061b198db8510271c1a
apptade <apptade64@gmail.com>
2026-07-18 20:28:36 +0300

    Create ForbiddenException.cs

---
+++ CR.Exceptions/ForbiddenException.cs
@@ -0,0 +1,8 @@
+﻿namespace CR.Exceptions;
+
+public abstract class ForbiddenException : CrException
+{
+    protected ForbiddenException(string code, string message, Exception? innerException = null) : base(code, message, innerException)
+    {
+    }
+}
