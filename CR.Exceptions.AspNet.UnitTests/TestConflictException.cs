c4bd0cf235875c59c0c3f227e9076f237a448680
apptade <apptade64@gmail.com>
2026-07-21 18:18:50 +0300

    Create ExceptionMappingOptionsTests.cs

---
+++ CR.Exceptions.AspNet.UnitTests/TestConflictException.cs
@@ -0,0 +1,11 @@
+﻿namespace CR.Exceptions.AspNet.UnitTests;
+
+public sealed class TestConflictException : ConflictException
+{
+    public TestConflictException() : base(
+        [
+            new Error("TestConflict", "Conflict")
+        ],
+        "Conflict")
+    { }
+}
---
+++ CR.Exceptions.AspNet.UnitTests/TestNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace CR.Exceptions.AspNet.UnitTests;
+
+public sealed class TestNotFoundException : NotFoundException
+{
+    public TestNotFoundException() : base(
+        [
+            new Error("TestNotFound", "Entity not found")
+        ],
+        "Entity not found")
+    { }
+}
