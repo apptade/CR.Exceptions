06b0d715957670977836d7ceed4ee3dde4f12106
apptade <apptade64@gmail.com>
2026-07-21 18:48:45 +0300

    Create CrExceptionHandlerTests.cs

---
+++ CR.Exceptions.AspNet.UnitTests/CrExceptionHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Xunit.Abstractions;
+
+namespace CR.Exceptions.AspNet.UnitTests;
+
+public sealed class CrExceptionHandlerTests
+{
+    private readonly ITestOutputHelper _output;
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+    public CrExceptionHandlerTests(ITestOutputHelper output)
+    {
+        _output = output;
+        _jsonSerializerOptions = new() { WriteIndented = true };
+    }
+
+    [Fact]
+    public async Task Should_Return_404_For_NotFoundException()
+    {
+        var services = new ServiceCollection()
+            .AddLogging()
+            .AddCrExceptionHandler();
+
+        var provider = services.BuildServiceProvider();
+        var handler = provider.GetRequiredService<IExceptionHandler>();
+
+        var context = new DefaultHttpContext { Response = { Body = new MemoryStream() } };
+        var result = await handler.TryHandleAsync(context, new TestNotFoundException(), CancellationToken.None);
+
+        context.Response.Body.Position = 0;
+        var jsonNode = await JsonNode.ParseAsync(context.Response.Body);
+        _output.WriteLine(jsonNode?.ToJsonString(_jsonSerializerOptions));
+
+        result.Should().BeTrue();
+        context.Response.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+    }
+}
