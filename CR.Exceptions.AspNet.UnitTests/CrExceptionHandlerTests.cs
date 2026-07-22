758cfd0d2150369d4e42f0801fe05c1bb116937b
apptade <apptade64@gmail.com>
2026-07-22 22:04:13 +0300

    Update CrExceptionHandlerTests.cs

--- CR.Exceptions.AspNet.UnitTests/CrExceptionHandlerTests.cs
+++ CR.Exceptions.AspNet.UnitTests/CrExceptionHandlerTests.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 using Xunit.Abstractions;
 
 namespace CR.Exceptions.AspNet.UnitTests;
@@ -11,12 +12,16 @@ namespace CR.Exceptions.AspNet.UnitTests;
 public sealed class CrExceptionHandlerTests
 {
     private readonly ITestOutputHelper _output;
-    private readonly JsonSerializerOptions _jsonSerializerOptions;
+    private readonly JsonSerializerOptions _jsonOptions;
 
     public CrExceptionHandlerTests(ITestOutputHelper output)
     {
         _output = output;
-        _jsonSerializerOptions = new() { WriteIndented = true };
+        _jsonOptions = new()
+        {
+            WriteIndented = true,
+            PropertyNameCaseInsensitive = true
+        };
     }
 
     [Fact]
@@ -51,6 +56,9 @@ public sealed class CrExceptionHandlerTests
 
         result.Should().BeTrue();
         context.Response.StatusCode.Should().Be(expectedStatusCode);
+
+        var problem = await DeserializeProblemDetails(context);
+        AssertProblemDetails(problem!, context, expectedStatusCode);
     }
 
     private static ServiceProvider CreateServiceProvider()
@@ -72,10 +80,48 @@ public sealed class CrExceptionHandlerTests
         };
     }
 
+    private async Task<ProblemDetailsResponse> DeserializeProblemDetails(HttpContext context)
+    {
+        context.Response.Body.Position = 0;
+
+        return (await JsonSerializer.DeserializeAsync<ProblemDetailsResponse>(
+            context.Response.Body, _jsonOptions))!;
+    }
+
     private async Task LogResponseBody(DefaultHttpContext context)
     {
         context.Response.Body.Position = 0;
         var jsonNode = await JsonNode.ParseAsync(context.Response.Body);
-        _output.WriteLine(jsonNode?.ToJsonString(_jsonSerializerOptions));
+        _output.WriteLine(jsonNode?.ToJsonString(_jsonOptions));
+    }
+
+    private static void AssertProblemDetails(ProblemDetailsResponse problem, HttpContext context, int expectedStatusCode)
+    {
+        problem.Should().NotBeNull();
+
+        problem.Type.Should().NotBeNullOrWhiteSpace();
+        problem.Title.Should().NotBeNullOrWhiteSpace();
+        problem.Detail.Should().NotBeNullOrWhiteSpace();
+
+        problem.Status.Should().Be(expectedStatusCode);
+        problem.Instance.Should().Be(context.Request.Path);
+
+        problem.TraceId.Should().NotBeNullOrWhiteSpace();
+        problem.Errors.Should().NotBeNull().And.NotBeEmpty();
+    }
+
+    private sealed class ProblemDetailsResponse
+    {
+        public string? Type { get; set; }
+        public string? Title { get; set; }
+        public int? Status { get; set; }
+        public string? Detail { get; set; }
+        public string? Instance { get; set; }
+
+        [JsonPropertyName(ProblemDetailsExtensionNames.TraceId)]
+        public string? TraceId { get; set; }
+
+        [JsonPropertyName(ProblemDetailsExtensionNames.Errors)]
+        public CrError[]? Errors { get; set; }
     }
 }
