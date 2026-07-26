e098221940c6c90890e06ebbeae415b812584b31
apptade <apptade64@gmail.com>
2026-07-26 18:33:39 +0300

    Delete TestConflictException.cs

--- CR.Exceptions.AspNet.UnitTests/TestConflictException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CR.Exceptions.AspNet.UnitTests;
-
-public sealed class TestConflictException : ConflictException
-{
-    public TestConflictException() : base([new("TestConflict", "Test Conflict")])
-    {
-    }
-}
