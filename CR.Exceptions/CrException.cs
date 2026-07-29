b111e4f92a9e2ef89b0aab38165bb2650dfed62c
apptade <apptade64@gmail.com>
2026-07-29 23:04:11 +0300

    Update CrException.cs

--- CR.Exceptions/CrException.cs
+++ CR.Exceptions/CrException.cs
@@ -9,24 +9,7 @@ public abstract class CrException : Exception
     protected CrException(ImmutableArray<CrError> errors, string message, Exception? innerException = null) : base(message, innerException)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(message);
-
-        if (errors.IsDefaultOrEmpty)
-        {
-            throw new ArgumentException("At least one error must be provided.", nameof(errors));
-        }
-
-        for (var i = 0; i < errors.Length; i++)
-        {
-            var error = errors[i];
-
-            ArgumentNullException.ThrowIfNull(error);
-
-            if (string.IsNullOrWhiteSpace(error.Code))
-                throw new ArgumentException($"errors[{i}].Code cannot be null or whitespace.", nameof(errors));
-
-            if (string.IsNullOrWhiteSpace(error.Message))
-                throw new ArgumentException($"errors[{i}].Message cannot be null or whitespace.", nameof(errors));
-        }
+        CrErrorValidator.ThrowExceptionIfInvalid(errors);
 
         Errors = errors;
     }
