66cc1e81a77db7c776b8a00540cc158d0b50b900
apptade <apptade64@gmail.com>
2026-08-01 14:15:22 +0300

    Add ImmutableArrayExtensions

---
+++ CR.Exceptions/Extensions/ImmutableArrayExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+
+namespace CR.Exceptions.Extensions;
+
+public static class ImmutableArrayExtensions
+{
+    extension<TSource>(ImmutableArray<TSource> source) where TSource : class?
+    {
+        public void ThrowIfEmptyOrContainsNull()
+        {
+            if (source.IsDefaultOrEmpty)
+            {
+                throw new ArgumentException("The array cannot be empty.", nameof(source));
+            }
+
+            for (var i = 0; i < source.Length; i++)
+            {
+                if (source[i] is null)
+                {
+                    throw new ArgumentNullException(nameof(source), $"The array element[{i}] is null.");
+                }
+            }
+        }
+    }
+}
