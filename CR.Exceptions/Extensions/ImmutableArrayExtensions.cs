7308c575bc365009edf82d08d948f945b18f9984
apptade <apptade64@gmail.com>
2026-08-01 21:29:21 +0300

    Update ImmutableArrayExtensions.cs

--- CR.Exceptions/Extensions/ImmutableArrayExtensions.cs
+++ CR.Exceptions/Extensions/ImmutableArrayExtensions.cs
@@ -1,23 +1,24 @@
 ﻿using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
 
 namespace CR.Exceptions.Extensions;
 
 public static class ImmutableArrayExtensions
 {
-    extension<TSource>(ImmutableArray<TSource> source) where TSource : class?
+    extension<TSource>(ImmutableArray<TSource> source)
     {
-        public void ThrowIfEmptyOrContainsNull()
+        public void ThrowIfEmptyOrContainsNull([CallerArgumentExpression(nameof(source))] string? paramName = null)
         {
             if (source.IsDefaultOrEmpty)
             {
-                throw new ArgumentException("The array cannot be empty.", nameof(source));
+                throw new ArgumentException("The array cannot be empty.", paramName);
             }
 
             for (var i = 0; i < source.Length; i++)
             {
                 if (source[i] is null)
                 {
-                    throw new ArgumentNullException(nameof(source), $"The array element[{i}] is null.");
+                    throw new ArgumentNullException(paramName, $"The array element[{i}] is null.");
                 }
             }
         }
