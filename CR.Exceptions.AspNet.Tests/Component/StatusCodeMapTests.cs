fdc326910e4fdd7314b91e3d80b8307ca2385594
apptade <apptade64@gmail.com>
2026-08-03 15:26:35 +0300

    Update asp tests

--- CR.Exceptions.AspNet.Tests/Component/StatusCodeMapTests.cs
+++ CR.Exceptions.AspNet.Tests/Component/StatusCodeMapTests.cs
@@ -1,35 +1,38 @@
 ﻿using CR.Exceptions.AspNet.Mapping;
+using CR.Exceptions.Tests;
 using Microsoft.AspNetCore.Http;
 
 namespace CR.Exceptions.AspNet.Tests.Component;
 
 public sealed class StatusCodeMapTests
 {
-    [Fact]
-    public void TryFind_ShouldReturn_404_For_NotFoundException()
-    {
-        var code = StatusCodes.Status404NotFound;
-        var map = CreateMap(builder => builder.Map<NotFoundException>(code));
+    private const int ExpectedStatusCode = StatusCodes.Status500InternalServerError;
+    private static readonly TestInternalException ExistentException = new();
+    private static readonly TestUnknownException NonExistentException = new();
 
-        var result = map.TryFind(new TestNotFoundException(), out var actualCode);
+    [Fact]
+    public void TryFind_ShouldReturn_TrueAndStatusCode_WhenExceptionExists()
+    {
+        var map = GetDefaultMap();
+        var result = map.TryFind(ExistentException, out var actualCode);
 
         Assert.True(result);
-        Assert.Equal(code, actualCode);
+        Assert.Equal(ExpectedStatusCode, actualCode);
     }
 
     [Fact]
-    public void TryFind_ShouldReturn_False_For_UnregisteredException()
+    public void TryFind_ShouldReturn_FalseAndDefault_WhenExceptionDoesNotExist()
     {
-        var map = CreateMap();
+        var map = GetDefaultMap();
+        var result = map.TryFind(NonExistentException, out var _);
 
-        Assert.False(map.TryFind(new TestUnregisteredException(), out var _));
+        Assert.False(result);
     }
 
-    private static StatusCodeMap CreateMap(Action<StatusCodeMapBuilder>? configurator = null)
+    private static StatusCodeMap GetDefaultMap()
     {
-        var builder = new StatusCodeMapBuilder();
-        configurator?.Invoke(builder);
-
-        return builder.Build();
+        return new StatusCodeMapBuilder()
+            .Map<TestInternalException>(ExpectedStatusCode)
+            .Build();
     }
 }
--- CR.Exceptions.AspNet.Tests/TestUnregisteredException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CR.Exceptions.AspNet.Tests;
-
-internal sealed class TestUnregisteredException : CrException
-{
-    public TestUnregisteredException() : base([new("TestUnregistered", "Test error message")], "Unregistered detail")
-    {
-    }
-}
